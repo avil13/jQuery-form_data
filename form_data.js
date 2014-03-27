c501364aa47baff6a8becfeeb2ef84e59419b641
avil13 <13.avil@gmail.com>
2014-03-27 17:35:02 +0300

    Update form_data.js

--- form_data.js
+++ form_data.js
@@ -1,9 +1,6 @@
 /*
-
-возвращает объект (ассоциированный массив) для отправки через ajax post данных
-
-select_attr атрибут по которому называются поля
-textvalue true/false если true берёт текст из выподающего списка, если false значение.
+form_data
+https://github.com/avil13/jQuery-form_data
 */
 
 (function($) {
@@ -69,30 +66,3 @@ textvalue true/false если true берёт текст из выподающе
 
 })(jQuery);
 
-
-/*
-//собрать данные из нескольких форм form=form_data(form_id1,select_attr1); form=form.concat(form_data(form_id2,select_attr2)); ... form- массив для post
-
-<form action="" id="frm">
-    <input type="text" name="txt">
-    <input type="submit" value="Send">
-</form>
-
-<script>
-
-jQuery(document).ready(function($) {
-    $('#frm').submit(function(){\
-
-        var v = $('#frm').form_data({
-            select_attr : 'name',
-            textvalue   : false
-        });
-
-        return false;
-    });
-});
-
-// returned:
-
-</script>
-*/
