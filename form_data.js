2c2c8c511dbd887dda56e65fc63ffba124139522
avil13 <a.1.3@mail.ru>
2014-08-28 14:37:22 +0400

    Add validation

--- form_data.js
+++ form_data.js
@@ -4,6 +4,8 @@
  */
 
 (function($) {
+   "use strict";
+
    $.fn.formData = function(options) {
 
       if (this === undefined) {
@@ -13,6 +15,8 @@
       var settings = $.extend({
          'select_attr': 'name',
          'textvalue': false,
+         'validator': false,
+         'invalid': false,
          'callback': false
       }, options);
 
@@ -79,9 +83,100 @@
          put($(j).attr('name'), $(j).attr('value'));
       });
 
-      // callback function
+      // validation  ==============================================
+      var checkValid = function(val, rule) {
+         var re;
+
+         switch (rule) {
+            case 'required':
+               return !(val === undefined || val.length === 0);
+
+            case 'email':
+               re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+               return re.test(val);
+
+            case 'phone':
+               re = /^\+?[-\(\)\d]{5,19}$/;
+               return re.test(val);
+
+            case 'url':
+               re = /^(https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)*?$/;
+               return re.test(val);
+
+            case 'alpha':
+               re = /^[a-zA-Z]+$/;
+               return re.test(val);
+
+            case 'alpha_num':
+               re = /^[a-zA-Z0-9]+$/;
+               return re.test(val);
+
+            case 'alpha_dash':
+               re = /^[-_a-zA-Z0-9]+$/;
+               return re.test(val);
+         }
+
+         if (rule.indexOf(':') > -1) {
+            rule = rule.split(':');
+            switch (rule[0]) {
+               case 'len':
+                  return rule.slice(1, rule.length).some(function(el) {
+                     return val.length == el;
+                  });
+
+               case 'range_len':
+                  return (val.length >= rule[1] && val.length <= rule[2]);
+
+               case 'max':
+                  return val <= rule[1];
+
+               case 'min':
+                  return val >= rule[1];
+
+               case 'range':
+                  return (val >= rule[1] && val <= rule[2]);
+
+               case 'not':
+                  return val != rule[1];
+
+               case 'is':
+                  return rule.slice(1, rule.length).some(function(el) {
+                     return val == send_obj[el];
+                  });
+
+               case 'same':
+                  return val == send_obj[rule[1]];
+
+               case 'diff':
+                  return val != send_obj[rule[1]];
+            }
+         }
+
+         return true;
+      };
+
+      var inValidEl = {};
+
+      if (settings.validator) {
+         for (var k in settings.validator) {
+            for (var i = 0; i < settings.validator[k].length; i++) {
+               if (!checkValid(send_obj[k], settings.validator[k][i])) {
+                  if (inValidEl[k] === undefined)
+                     inValidEl[k] = [];
+                  inValidEl[k].push(settings.validator[k][i].split(':')[0]);
+               }
+            }
+         }
+      }
+
+      if (!$.isEmptyObject(inValidEl) && settings.invalid) {
+         settings.invalid(inValidEl);
+      }
+
+
+      // callback function =========================================
       if (settings.callback) {
-         settings.callback(send_obj);
+         settings.callback(send_obj, inValidEl);
       }
 
       return send_obj;
